baa5fd9b853b7aa417a1a9530f7d8f17af60fa3d
niexiao <niexiao_giser@163.com>
2016-03-12 14:47:58 +0800

    添加相关表前的准备工作

--- src/com/niexiao/entity/base/UserEntity.java
+++ src/com/niexiao/entity/base/UserEntity.java
@@ -1,18 +1,11 @@
 package com.niexiao.entity.base;
 
-import java.util.UUID;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import com.niexiao.base.entity.BaseEntity;
 import com.niexiao.entity.Organization;
-import com.niexiao.entity.UserExt;
 
 
 @MappedSuperclass
